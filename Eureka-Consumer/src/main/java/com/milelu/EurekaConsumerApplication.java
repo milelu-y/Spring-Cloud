497cbb33e9c04bfd1d93ae7800a974d56d919afc
1912655018 <1912655018@qq.com>
2021-02-21 17:07:22 +0800

    消费端 提供端

---
+++ Eureka-Consumer/src/main/java/com/milelu/EurekaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.milelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConsumerApplication.class, args);
+    }
+
+}
